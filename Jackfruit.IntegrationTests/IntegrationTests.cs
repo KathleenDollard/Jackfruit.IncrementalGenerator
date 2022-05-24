cce30ef5205c9e10188a60daebc5f7332aa35827
Kathleen Dollard <kdollard@microsoft.com>
2022-05-24 08:18:43 -0400

    Integration test using `dotnet build`

--- Jackfruit.IntegrationTests/IntegrationTests.cs
+++ Jackfruit.IntegrationTests/IntegrationTests.cs
@@ -1,6 +1,7 @@
 using Jackfruit.IncrementalGenerator;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Emit;
 using System.Diagnostics;
 
@@ -30,181 +31,115 @@ namespace Jackfruit.Tests
             return outputCompilation;
         }
 
-        private static EmitResult TestGenerationEmit(Compilation outputCompilation, string outputPath)
+        private static void TestOutput(Compilation generatedCompilation)
         {
-            var emitResult = outputCompilation.Emit(outputPath);
-            Assert.Empty(emitResult.Diagnostics.Where(x => x.Severity is DiagnosticSeverity.Error or DiagnosticSeverity.Warning));
-            Assert.True(emitResult.Success);
-            return emitResult;
+            foreach (var tree in generatedCompilation.SyntaxTrees)
+            {
+                var className = !string.IsNullOrWhiteSpace(tree.FilePath)
+                        ? Path.GetFileName(tree.FilePath)
+                        : tree.GetRoot().DescendantNodes()
+                            .OfType<ClassDeclarationSyntax>()
+                            .First()
+                            .Identifier.ToString() + ".cs";
+                if (className != "Program.cs" && className != "Handlers.cs" && className != "Validators.cs")
+                {
+                    var fileName = Path.Combine(testGeneratedCodePath, className);
+                    File.WriteAllText(fileName, tree.ToString());
+                }
+            }
+            Assert.Equal(3, Directory.GetFiles(testGeneratedCodePath).Count());
+        }
+
+        private const string dotnetVersion = "net6.0";
+        private const string testSetName = "TestOutputExample";
+        private const string testInputPath = @$"..\..\..\..\{testSetName}";
+        private static string testGeneratedCodePath = Path.Combine(testInputPath, "GeneratedViaTest");
+        private static string testBuildPath = Path.Combine(testInputPath, "bin", "Debug", dotnetVersion);
+        private static string handlerFilePath = Path.Combine(testInputPath, "Handlers.cs");
+        private static string validatorFilePath = Path.Combine(testInputPath, "Validators.cs");
+        private static string programFilePath = Path.Combine(testInputPath, "Program.cs");
+
+        private static SyntaxTree HandlerSyntaxTree => CSharpSyntaxTree.ParseText(File.ReadAllText(handlerFilePath));
+        private static SyntaxTree ValidatorSyntaxTree => CSharpSyntaxTree.ParseText(File.ReadAllText(validatorFilePath));
+        private static SyntaxTree ProgramSyntaxTree => CSharpSyntaxTree.ParseText(File.ReadAllText(programFilePath));
+        private static Process? TestOutputCompiles()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            ////startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.WorkingDirectory = testInputPath;
+            startInfo.FileName = "dotnet";
+            startInfo.Arguments = "build";
+            Process? exeProcess = Process.Start(startInfo);
+            Assert.NotNull(exeProcess);
+            if (exeProcess is not null)
+            {
+                exeProcess.WaitForExit();
+
+                var output = exeProcess.StandardOutput.ReadToEnd();
+                var error = exeProcess.StandardError.ReadToEnd();
+
+                Assert.Equal(0, exeProcess.ExitCode);
+                Assert.Equal("", error);
+            }
+
+            return exeProcess;
         }
 
         [Fact]
-        public void Example_compiles_without_error()
+        public void Example_compiles()
         {
-            var compilation = TestCreatingCompilation(Code.ProgramSyntaxTree, Code.HandlerSyntaxTree, Code.ValidatorSyntaxTree);
+            var compilation = TestCreatingCompilation(ProgramSyntaxTree, HandlerSyntaxTree, ValidatorSyntaxTree);
             TestGeneration(compilation, new Generator());
         }
 
+        [Fact]
+        public void Example_outputs()
+        {
+            var compilation = TestCreatingCompilation(ProgramSyntaxTree, HandlerSyntaxTree, ValidatorSyntaxTree);
+            var outputCompilation = TestGeneration(compilation, new Generator());
+            TestOutput(outputCompilation);
+        }
 
+        [Fact]
+        public void Example_output_builds()
+        {
+            var compilation = TestCreatingCompilation(ProgramSyntaxTree, HandlerSyntaxTree, ValidatorSyntaxTree);
+            var outputCompilation = TestGeneration(compilation, new Generator());
+            TestOutput(outputCompilation);
+            TestOutputCompiles();
+        }
 
-        //[Fact]
-        //public void Example_emits_without_error()
-        //{
-        //    var compilation = TestCreatingCompilation(Code.ProgramSyntaxTree, Code.HandlerSyntaxTree, Code.ValidatorSyntaxTree);
-        //    var outputCompilation = TestGeneration(compilation, new Generator());
-        //    TestGenerationEmit(outputCompilation, @".\Test.dll");
-        //}
+        [Fact]
+        public void Example_output_runs()
+        {
+            var compilation = TestCreatingCompilation(ProgramSyntaxTree, HandlerSyntaxTree, ValidatorSyntaxTree);
+            var outputCompilation = TestGeneration(compilation, new Generator());
+            TestOutput(outputCompilation);
+            TestOutputCompiles();
 
-        //[Fact]
-        //public void Example_runs_without_error()
-        //{
-        //    var compilation = TestCreatingCompilation(Code.ProgramSyntaxTree, Code.HandlerSyntaxTree, Code.ValidatorSyntaxTree);
-        //    var outputCompilation = TestGeneration(compilation, new Generator());
-        //    var file = $@".\{Path.GetTempFileName()}.dll";
-        //    TestGenerationEmit(outputCompilation,file);
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.FileName = $"{Path.Combine(testBuildPath, testSetName)}.exe";
+            startInfo.Arguments = "StarTrek --Uhura";
 
-        //    // Output test.runtimeconfig.json
+            using Process? exeProcess = Process.Start(startInfo);
+            Assert.NotNull(exeProcess);
+            if (exeProcess is not null)
+            {
+                exeProcess.WaitForExit();
 
-        //    ProcessStartInfo startInfo = new ProcessStartInfo();
-        //    ////startInfo.CreateNoWindow = false;
-        //    startInfo.UseShellExecute = false;
-        //    startInfo.RedirectStandardOutput = true;
-        //    startInfo.RedirectStandardError = true;
-        //    startInfo.FileName = file;
-        //    startInfo.Arguments = "StarTrek --Uhura";
+                var output = exeProcess.StandardOutput.ReadToEnd();
+                var error = exeProcess.StandardError.ReadToEnd();
 
-        //    using Process? exeProcess = Process.Start(startInfo);
-        //    if (exeProcess is not null)
-        //    { exeProcess.WaitForExit(); }
-
-        //    var output = exeProcess.StandardOutput.ReadToEnd();
-        //    var error = exeProcess.StandardError.ReadToEnd();
-
-        //    Assert.Equal(0, exeProcess.ExitCode);
-        //    Assert.Equal("Hello, Nyota Uhura", output);
-        //    Assert.Equal("", error);
-        //}
+                Assert.Equal(0, exeProcess.ExitCode);
+                Assert.Equal("Hello, Nyota Uhura\r\n", output);
+                Assert.Equal("", error);
+            }
+        }
     }
 }
-
-//class Program
-//{
-//    static void Main()
-//    {
-//        LaunchCommandLineApp();
-//    }
-
-//    /// <summary>
-//    /// Launch the application with some options set.
-//    /// </summary>
-//    static void LaunchCommandLineApp()
-//    {
-//        // For the example
-//        const string ex1 = "C:\\";
-//        const string ex2 = "C:\\Dir";
-
-//        // Use ProcessStartInfo class
-//        ProcessStartInfo startInfo = new ProcessStartInfo();
-//        startInfo.CreateNoWindow = false;
-//        startInfo.UseShellExecute = false;
-//        startInfo.FileName = "dcm2jpg.exe";
-//        startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-//        startInfo.Arguments = "-f j -o \"" + ex1 + "\" -z 1.0 -s y " + ex2;
-
-//        try
-//        {
-//            // Start the process with the info we specified.
-//            // Call WaitForExit and then the using statement will close.
-//            using (Process exeProcess = Process.Start(startInfo))
-//            {
-//                exeProcess.WaitForExit();
-//            }
-//        }
-//        catch
-//        {
-//            // Log error.
-//        }
-//    }
-//}
-
-//namespace CompilationTest
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            HashSet<Assembly> referencedAssemblies = new HashSet<Assembly>()
-//            {
-//                typeof(object).Assembly,
-//                Assembly.Load(new AssemblyName("Microsoft.CSharp")),
-//                Assembly.Load(new AssemblyName("netstandard")),
-//                Assembly.Load(new AssemblyName("System.Runtime")),
-//                Assembly.Load(new AssemblyName("System.Linq")),
-//                Assembly.Load(new AssemblyName("System.Linq.Expressions"))
-//            };
-
-//            string greetingClass = @"
-//namespace TestLibraryAssembly
-//{
-//    public static class Greeting
-//    {
-//        public static string GetGreeting(string name)
-//        {
-//            return ""Hello, "" + name + ""!"";
-//        }
-//    }
-//}
-//";
-
-//            CSharpCompilation compilation1 = CSharpCompilation.Create(
-//                "TestLibraryAssembly",
-//                new[]
-//                {
-//                    CSharpSyntaxTree.ParseText(greetingClass)
-//                },
-//                referencedAssemblies.Select(assembly => MetadataReference.CreateFromFile(assembly.Location)).ToList(),
-//                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
-//            );
-//            MemoryStream memoryStream1 = new MemoryStream();
-//            EmitResult emitResult1 = compilation1.Emit(memoryStream1);
-//            memoryStream1.Position = 0;
-//            MetadataReference testLibraryReference = MetadataReference.CreateFromStream(memoryStream1);
-
-//            string programCode = @"
-//using TestLibraryAssembly;
-
-//namespace TestProgram
-//{
-//    public class Program
-//    {
-//        public void Main()
-//        {
-//            string greeting = Greeting.GetGreeting(""Name"");
-//        }
-//    }
-//}
-//";
-
-//            CSharpCompilation compilation2 = CSharpCompilation.Create(
-//                "TestProgram",
-//                new[]
-//                {
-//                    CSharpSyntaxTree.ParseText(programCode)
-//                },
-//                referencedAssemblies
-//                    .Select(assembly => MetadataReference.CreateFromFile(assembly.Location))
-//                    .Concat(new List<MetadataReference> { testLibraryReference }).ToList(),
-//                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
-//            );
-//            MemoryStream memoryStream2 = new MemoryStream();
-//            EmitResult emitResult2 = compilation2.Emit(memoryStream2);
-//            memoryStream2.Position = 0;
-
-//            Assembly programAssembly = Assembly.Load(memoryStream2.ToArray());
-//            Type programType = programAssembly.GetType("TestProgram.Program");
-//            MethodInfo method = programType.GetMethod("Main");
-//            object instance = Activator.CreateInstance(programType);
-//            method.Invoke(instance, null);
-//        }
-//    }
-//}
